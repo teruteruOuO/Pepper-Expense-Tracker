119b968d18771c235a642da7ad6963341b8bdedd
asangel2076005 <asangel@cougarnet.uh.edu>
2025-02-04 17:07:42 -0600

    Added GET request for basic user information in Account Page
    - Developed a GET request method asking the backend and database server for user's basic information (first and last name, initial, email, and settings) each time a user goes to the Account page

--- frontend/src/views/AccountView.vue
+++ frontend/src/views/AccountView.vue
@@ -1,12 +1,14 @@
 <template>
 <main class="account-view">
     <h1>Account Page</h1>
+    <UpdateAccountComponent />
     <LogoutComponent />
 </main>
 </template>
 
 
 <script setup>
+import UpdateAccountComponent from '@/components/one-time/UpdateAccountComponent.vue';
 import LogoutComponent from '@/components/one-time/LogoutComponent.vue';
 </script>
 
