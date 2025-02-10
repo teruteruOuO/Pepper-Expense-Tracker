4a89256461da43025230f3582c68d2b64c7bfa92
YukichiOhno <juuzousuzuya200@gmail.com>
2025-02-10 16:35:49 -0600

    - Developed Username Change System
    - Added constraints in the database that disables user to use special characters or white spaces in their username. Only letters, numbers, and underscores are allowed to be used for usernames

--- frontend/src/views/AccountView.vue
+++ frontend/src/views/AccountView.vue
@@ -3,6 +3,7 @@
     <h1>Account Page</h1>
     <UpdateProfileComponent />
     <PasswordChangeComponent />
+    <UsernameChangeComponent />
     <LogoutComponent />
 </main>
 </template>
@@ -11,6 +12,7 @@
 <script setup>
 import UpdateProfileComponent from '@/components/one-time/account/UpdateProfileComponent.vue';
 import PasswordChangeComponent from '@/components/one-time/account/PasswordChangeComponent.vue';
+import UsernameChangeComponent from '@/components/one-time/account/UsernameChangeComponent.vue';
 import LogoutComponent from '@/components/one-time/account/LogoutComponent.vue';
 </script>
 
